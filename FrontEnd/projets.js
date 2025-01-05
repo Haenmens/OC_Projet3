02e52fc5d1c747b7f37a768492fcca567750f4dc
unknown <clamens.pierre@yahoo.fr>
2025-01-05 11:27:42 +0100

    Finalisation des fonctionnalités et de l'affichage de la modale

--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -1,6 +1,7 @@
 categorieAffichee = "tous";
 travaux = null;
 categories = null;
+premierAffichageModale = true;
 
 async function chargerTravaux(categorie) {
     try 
@@ -96,7 +97,8 @@ function verifierPhoto(photo)
     {
         if (photo.type != "image/jpeg" && photo.type != "image/png")
         {
-            alert("Veuillez sélectionner un fichier de type jpg ou png.")
+            alert("Veuillez sélectionner un fichier de type jpg ou png.");
+            return;
         }
 
         const lecteur = new FileReader();
@@ -113,6 +115,152 @@ function verifierPhoto(photo)
     }
 }
 
+function verifierValiditeFormulaire(formulaire)
+{
+    if (formulaire.checkValidity())
+    {
+        document.getElementById("bouton-formulaire-ajout").classList.add("bouton-ajouter-autorise");
+    }
+    else
+    {
+        document.getElementById("bouton-formulaire-ajout").classList.remove("bouton-ajouter-autorise");
+    }
+}
+
+async function ajouterPhotoBaseDeDonnee(formulaire)
+{
+    const donneesFormulaire = new FormData(formulaire);
+
+    console.log(donneesFormulaire);
+
+    try {
+        const reponse = await fetch("http://localhost:5678/api/works",
+            {
+                method: "POST",
+                headers: {"Authorization" : `Bearer ${ sessionStorage.getItem("token") }`},
+                body: donneesFormulaire
+            }
+        );
+
+        if (reponse.ok)
+        {
+            const data = await reponse.json();
+            chargerTravaux(categorieAffichee);
+            alert(`La photo intitulée : ${donneesFormulaire.get("title")} a bien été ajoutée aux travaux !`);
+            fermerModale(document.getElementById("modale"), formulaire);
+        } 
+        else
+        {
+            const data = await reponse.json();
+            console.log(data)
+        }
+    }
+    catch (erreur) {
+        console.log("Erreur dans l'envoi' : ", erreur);
+    }
+}
+
+async function supprimerPhotoBaseDeDonnee(id) 
+{
+    try {
+        const reponse = await fetch(`http://localhost:5678/api/works/${id}`,
+            {
+                method: "DELETE",
+                headers: {"Authorization" : `Bearer ${ sessionStorage.getItem("token") }`}
+            }
+        );
+
+        if (reponse.ok)
+        {
+            await chargerTravaux(categorieAffichee);
+            afficherPhotosModale();
+        } 
+        else
+        {
+            const data = await reponse.json();
+            console.log(data)
+        }
+    }
+    catch (erreur) {
+        console.log("Erreur dans la suppression' : ", erreur);
+    }
+}
+
+function resetFormulaire(formulaire)
+{
+    imagePhoto = document.getElementById("formulaire-ajout-image-photo");
+
+    imagePhoto.setAttribute("src", "assets/icons/image.png");
+    imagePhoto.style.width = "68px";
+    imagePhoto.style.height = "60px";
+    document.getElementById("formulaire-ajout-bouton-photo").classList.remove("cachee");
+    document.getElementById("formulaire-ajout-prerequis-photo").classList.remove("cachee");
+    document.getElementById("bouton-formulaire-ajout").classList.remove("bouton-ajouter-autorise");
+
+    formulaire.reset();
+}
+
+function fermerModale(modale, formulaireAjout)
+{
+    modale.classList.remove("modale");
+    modale.classList.add("cachee");
+    document.getElementById("fenetre-modale-modif").classList.remove("cachee");
+    document.getElementById("fenetre-modale-ajout").classList.add("cachee");
+    resetFormulaire(formulaireAjout);
+}
+
+function afficherFormulaireAjout()
+{
+    document.getElementById("fenetre-modale-modif").classList.add("cachee");
+    document.getElementById("fenetre-modale-ajout").classList.remove("cachee");
+    document.getElementById("fenetre-modale-ajout").classList.add("fenetre-modale-ajout");
+
+    formulaireAjoutCategories = document.getElementById("formulaire-ajout-categorie");
+    formulaireAjoutCategories.innerHTML = '<option value="" hidden selected></option>';
+
+    for (let i = 0; i < categories.length; i++)
+    {
+        const categorie = categories[i];
+
+        const elementOption = document.createElement("option");
+        elementOption.innerHTML = categorie.name;
+        elementOption.value = categorie.id;
+
+        formulaireAjoutCategories.appendChild(elementOption);
+    }
+}
+
+function afficherPhotosModale()
+{
+    photos = document.getElementById("photos");
+    photos.innerHTML = "";
+
+    for (let i = 0; i < travaux.length; i++)
+    {
+        const travail = travaux[i];
+        const id = travail.id;
+
+        const elementDiv = document.createElement("div");
+        const elementImage = document.createElement("img");
+        const elementIconeSupprimer = document.createElement("img");
+
+        elementDiv.style = "position: relative;";
+
+        elementImage.src = travail.imageUrl;
+        elementImage.classList.add("fenetre-modale-modif-photo");
+
+        elementIconeSupprimer.src = "assets/icons/poubelle.png";
+        elementIconeSupprimer.classList.add("photos-icone-supprimer");
+        elementIconeSupprimer.addEventListener("click", () => {
+            supprimerPhotoBaseDeDonnee(id);
+        });
+
+        photos.appendChild(elementDiv);
+        elementDiv.appendChild(elementImage);
+        elementDiv.appendChild(elementIconeSupprimer);
+    }
+}
+
 function afficherModale()
 {
     modale = document.getElementById("modale");
@@ -121,58 +269,57 @@ function afficherModale()
     boutonPhoto = document.getElementById("input-photo");
     boutonModifVersAjout = document.getElementById("bouton-modif-vers-ajout");
     boutonAjoutVersModif = document.getElementById("bouton-ajout-vers-modif");
+    formulaireAjout = document.getElementById("formulaire-ajout");
 
-    modale.addEventListener("click", (event) => {
-        if (event.target == modale)
-        {
-            modale.classList.remove("modale");
-            modale.classList.add("cachee");
-        }
-    });
-
-    croixImg.addEventListener("click", () => {
-        modale.classList.remove("modale");
-        modale.classList.add("cachee");
-    });
-
-    boutonModifVersAjout.addEventListener("click", () => {
-        document.getElementById("fenetre-modale-modif").classList.add("cachee");
-        document.getElementById("fenetre-modale-ajout").classList.remove("cachee");
-        document.getElementById("fenetre-modale-ajout").classList.add("fenetre-modale-ajout");
-    });
-
-    boutonAjoutVersModif.addEventListener("click", () => {
-        document.getElementById("fenetre-modale-modif").classList.remove("cachee");
-        document.getElementById("fenetre-modale-ajout").classList.add("cachee");
-    });
-
-    boutonPhoto.addEventListener("change", () => { verifierPhoto(boutonPhoto.files[0]); });
-
-    photos.addEventListener("wheel", function (event) {
-        const scrollTop = photos.scrollTop;
-        const scrollHauteur = photos.scrollHeight;
-        const clientHauteur = photos.clientHeight;
-        const delta = event.deltaY;
-
-        if ((delta > 0 && scrollTop + clientHauteur >= scrollHauteur) ||
-            (delta < 0 && scrollTop <= 0))
-            {
-                event.preventDefault();
-            }
-    });
-
-    photos.innerHTML = "";
-
-    for (let i = 0; i < travaux.length; i++)
+    if (premierAffichageModale)
     {
-        travail = travaux[i];
-        
-        const elementImage = document.createElement("img");
+        modale.addEventListener("click", (event) => {
+            if (event.target == modale)
+            {
+                fermerModale(modale, formulaireAjout);
+            }
+        });
 
-        elementImage.src = travail.imageUrl;
+        croixImg.addEventListener("click", () => {
+            fermerModale(modale, formulaireAjout);
+        });
 
-        photos.appendChild(elementImage);
+        boutonModifVersAjout.addEventListener("click", () => {
+            afficherFormulaireAjout();
+        });
+
+        boutonAjoutVersModif.addEventListener("click", () => {
+            document.getElementById("fenetre-modale-modif").classList.remove("cachee");
+            document.getElementById("fenetre-modale-ajout").classList.add("cachee");
+            resetFormulaire(formulaireAjout);
+            afficherPhotosModale();
+        });
+
+        boutonPhoto.addEventListener("change", () => { verifierPhoto(boutonPhoto.files[0]); });
+
+        formulaireAjout.addEventListener("input", () => { verifierValiditeFormulaire(formulaireAjout); });
+        formulaireAjout.addEventListener("submit", (event) => { 
+            event.preventDefault();
+            ajouterPhotoBaseDeDonnee(formulaireAjout);
+        });
+
+        photos.addEventListener("wheel", function (event) {
+            const scrollTop = photos.scrollTop;
+            const scrollHauteur = photos.scrollHeight;
+            const clientHauteur = photos.clientHeight;
+            const delta = event.deltaY;
+
+            if ((delta > 0 && scrollTop + clientHauteur >= scrollHauteur) ||
+                (delta < 0 && scrollTop <= 0))
+                {
+                    event.preventDefault();
+                }
+        });
+
+        premierAffichageModale = false;
     }
+
+    afficherPhotosModale();
 
     modale.classList.remove("cachee");
     modale.classList.add("modale")
@@ -206,18 +353,20 @@ function verifierConnexion()
 
     boutonModifier.classList.remove("cachee");
     boutonModifier.classList.add("bouton-modifier");
-    boutonModifier.addEventListener("click", afficherModale);
 
     bandeauEdition.classList.remove("cachee");
     bandeauEdition.classList.add("bandeau-edition");
 
     logoutLien.classList.remove("cachee");
-    logoutLien.addEventListener("click", function() {
-        sessionStorage.removeItem("token");
-        window.location.assign("index.html");
-    });
 }
 
 chargerTravaux("tous");
 chargerFiltres();
 verifierConnexion();
+
+logoutLien.addEventListener("click", function() {
+    sessionStorage.removeItem("token");
+    window.location.assign("index.html");
+});
+
+boutonModifier.addEventListener("click", afficherModale);
